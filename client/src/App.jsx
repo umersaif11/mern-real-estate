051d8d74cfa15105a30691a04c56d28752361518
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-13 21:08:52 +0500

    remove bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import Home from "./pages/Home";
 
-Routesimport { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return 
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
