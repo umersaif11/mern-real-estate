0b6d3c8fda23b72824da9dda32d957842093b481
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-14 20:09:10 +0500

    Create title and then form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="font-bold text-sm sm:text-xl flex flex-wrap">
+      <span className="text-slate-500">Sahand</span>
+      <span className="text-slate-700">Estate</span>
+      <form>
+        <input type="text" placeholder="Search..." />
+      </form>
+    </header>
+  );
 };
 
 export default Header;
