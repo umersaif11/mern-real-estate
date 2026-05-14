948496055bfdd22052c6c276feadd31fd5fcc11a
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-14 21:27:12 +0500

    Install react icons and get search icon

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -8,8 +8,13 @@ const Header = () => {
           <span className="text-slate-500">Sahand</span>
           <span className="text-slate-700">Estate</span>
         </h1>
-        <form>
-          <input type="text" placeholder="Search..." />
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
         </form>
       </div>
     </header>
