9f602075490d9799f80ce340423c28baa2c18538
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-14 20:28:53 +0500

    Move form and title into another div and make it flexbox, items centre and having space around them

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="font-bold text-sm sm:text-xl flex flex-wrap">
-      <span className="text-slate-500">Sahand</span>
-      <span className="text-slate-700">Estate</span>
-      <form>
-        <input type="text" placeholder="Search..." />
-      </form>
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-slate-500">Sahand</span>
+          <span className="text-slate-700">Estate</span>
+        </h1>
+        <form>
+          <input type="text" placeholder="Search..." />
+        </form>
+      </div>
     </header>
   );
 };
