5e1488be4378328debf237823b2b71ea1da09d5a
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-15 19:37:27 +0500

    Whenever user clicks icon, the page should navigate to homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">Sahand</span>
-          <span className="text-slate-700">Estate</span>
-        </h1>
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Sahand</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
@@ -17,9 +20,15 @@ const Header = () => {
           <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-4">
-          <li className="hidden sm:inline">Home</li>
-          <li>About</li>
-          <li>Sign In</li>
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            Home
+          </li>
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            About
+          </li>
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            Contact
+          </li>
         </ul>
       </div>
     </header>
