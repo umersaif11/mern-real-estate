1a6aee3dfee4efcb1b749be87390f9c2c2256cfb
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-23 18:52:48 +0500

    Connect to database using env variable and install dotenv package

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
 
