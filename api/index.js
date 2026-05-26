51229ac597bf3e2e44cf4c9c3f39a534b00f68aa
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-26 19:03:52 +0500

    Check if connected to database

--- api/index.js
+++ api/index.js
@@ -3,7 +3,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 const app = express();
 
